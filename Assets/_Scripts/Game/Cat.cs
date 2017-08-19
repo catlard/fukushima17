dac8145b33413a6fc7b6af1b78884b2675bbf272
ChaseLee <cv13399@gmail.com>
2017-08-19 18:21:14 +0800

    add cat and merge

---
+++ Assets/_Scripts/Game/Cat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+	public SpriteRenderer face;
+	public List <Sprite> spriteList = new List<Sprite> ();
+
+	public void startAnimation()
+	{
+		
+	}
+
+	public void stopAnimation()
+	{
+		
+	}
+		
+}
