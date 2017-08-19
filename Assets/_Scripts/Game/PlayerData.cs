b06f54be6cdd5b5c5beba36dfde0060a8969351c
Heng-Yi,Chien (ian) <iantttiantttianttt@gmail.com>
2017-08-19 20:08:40 +0800

    player data update

--- Assets/_Scripts/Game/PlayerData.cs
+++ Assets/_Scripts/Game/PlayerData.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 [System.Serializable]
 public struct PlayerData
 {
-    public List<KeyCode> player_code;// press down keycode list
+	public int score;
+	public GameObject StylePrefab;
+	public KeyCode player_code;// press down keycode list
 	public Color _color;
 
 }
--- Assets/_Scripts/Result/ResultModel.cs
+++ Assets/_Scripts/Result/ResultModel.cs
@@ -7,7 +7,12 @@ public class ResultModel : SingletonMonoBehavior<ResultModel> {
 	private ResultDeta ResultDeta;
 	[SerializeField]
 	private bool IsStarted;
+	private int maxScore;
 
+	[SerializeField]
+	public List<PlayerData> Players;
+	public List<GameObject> PlayerObject;
+	public List<int> CurPlayerCheeseCount;
 
 	public void Init() {
 		IsStarted  = false;
@@ -31,15 +36,63 @@ public class ResultModel : SingletonMonoBehavior<ResultModel> {
 		IsStarted = _IsStarted;
 	}
 
+
 	public void LoadGameResult () {
 		Debug.Log ("Load Game Result.");
+		PlayerObject.Clear ();
+		Players.Clear ();
+		Players = ResultDeta.players;
 	}
 
-	public void InitResultAssest () {
-		Debug.Log ("Init Result Assest.");
+
+	public void CreatPlayerAssets () {
+		int curPlayerCount = 0;
+		int maxSortingOrder = (ResultModel.instance.Players.Count / 8) + 2;
+		for (int i = 0; i < ResultModel.instance.Players.Count; i++) {
+			if ((curPlayerCount % 8) % 2 == 0) {
+				if ((curPlayerCount / 8) % 2 == 0) {
+					GameObject player = Instantiate(Players[i].StylePrefab, new Vector3((MOUSE_WIDTH/2f)+(MOUSE_WIDTH * (curPlayerCount % 8)/2f),(curPlayerCount / 8)*MOUSE_HIGHT + MOUSE_Y_OFFSET,0f),Quaternion.identity);
+					PlayerObject.Add (player);
+					player.GetComponent<SpriteRenderer> ().sortingOrder = maxSortingOrder - (curPlayerCount / 8);
+				}else if((curPlayerCount / 8) % 2 == 1) {
+					GameObject player = Instantiate(Players[i].StylePrefab, new Vector3((MOUSE_WIDTH * (curPlayerCount % 8)/2f),(curPlayerCount / 8)*MOUSE_HIGHT + MOUSE_Y_OFFSET,0f),Quaternion.identity);
+					PlayerObject.Add (player);
+					player.GetComponent<SpriteRenderer> ().sortingOrder = maxSortingOrder - (curPlayerCount / 8);
+				}	
+			}else if ((curPlayerCount % 8) % 2 == 1) {
+				if ((curPlayerCount / 8) % 2 == 0) {
+					GameObject player = Instantiate(Players[i].StylePrefab, new Vector3(0f-(MOUSE_WIDTH * (curPlayerCount % 8)/2f),(curPlayerCount / 8)*MOUSE_HIGHT + MOUSE_Y_OFFSET,0f),Quaternion.identity);
+					PlayerObject.Add (player);
+					player.GetComponent<SpriteRenderer> ().sortingOrder = maxSortingOrder - (curPlayerCount / 8);
+				}else if((curPlayerCount / 8) % 2 == 1) {
+					GameObject player = Instantiate(Players[i].StylePrefab, new Vector3(0f-(MOUSE_WIDTH * (curPlayerCount % 8)/2f)-(MOUSE_WIDTH/2f),(curPlayerCount / 8)*MOUSE_HIGHT + MOUSE_Y_OFFSET,0f),Quaternion.identity);
+					PlayerObject.Add (player);
+					player.GetComponent<SpriteRenderer> ().sortingOrder = maxSortingOrder - (curPlayerCount / 8);
+				}	
+			}
+			curPlayerCount++;
+		}
 	}
 
-	public void StartPlayResult () {
-		Debug.Log ("Start Play Result.");
+
+	public void PlusCheese (KeyCode _kcode) {
+		for (int i = 0; i < Players.Count; i++) {
+			if (Players [i].player_code == _kcode) {
+				if (Players [i].score < CurPlayerCheeseCount [i]) {
+					//Create Cheese
+
+				}
+			}
+		}
 	}
+
+
+
+
+
+	private const float MOUSE_WIDTH = 2.5f;
+	private const float MOUSE_HIGHT = 1.0f;
+	private const float MOUSE_Y_OFFSET = -2f;
+	private const float CHEESE_HIGHT = 1.0f;
+	private const float CHEESE_Y_OFFSET = 0.5f;
 }
