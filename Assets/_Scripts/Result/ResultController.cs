a409e380365b0b6b510e09c10547193b47b8c993
Heng-Yi,Chien (ian) <iantttiantttianttt@gmail.com>
2017-08-19 16:23:14 +0800

    Basic result scene

---
+++ Assets/_Scripts/Result/ResultController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultController : SingletonMonoBehavior<ResultController> {
+
+
+	void Start () {
+		ResultModel.instance.Init ();
+		ResultView.instance.Init ();
+		if (ResultModel.instance.GetHaveResult () == true && ResultModel.instance.GetIsStarted() == false) {
+			ResultModel.instance.InitResultAssest ();
+			ResultModel.instance.StartPlayResult ();
+			ResultModel.instance.SetIsStarted (true);
+		}
+	}
+
+
+	void Update () {
+
+
+	}
+
+
+}
