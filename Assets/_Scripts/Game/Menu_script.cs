eb73820d5a3e012a13cd6a2407c6fa3a3bb11469
ivanl1234 <leeholun1023@gmail.com>
2017-08-19 20:10:12 +0800

    sorry guys

--- Assets/_Scripts/Game/Menu_script.cs
+++ Assets/_Scripts/Game/Menu_script.cs
@@ -6,17 +6,14 @@ using UnityEngine.UI;
 
 public class Menu_script : MonoBehaviour
 {
-<<<<<<< HEAD
     public bool alphabetlock = false;
 	public List<PlayerData> _players;
     public GameObject test_object;
 
-=======
     PlayerData data = new PlayerData();
->>>>>>> 0cf2bb9bb665de4924a12d39676e10aeb95d3253
     void Start()
     {
-        data.player_code = new List<KeyCode>();
+        _players = new List<PlayerData> ();
     }
 
     public void Update()
@@ -24,7 +21,6 @@ public class Menu_script : MonoBehaviour
 
         foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))//Get what key are you pressing
         {
-<<<<<<< HEAD
 			if (Input.GetKeyDown (kcode))
            {
                 if((int)kcode==27)//get escape key
@@ -58,41 +54,22 @@ public class Menu_script : MonoBehaviour
 				}
                 }
 
-=======
-            if (Input.GetKeyDown(kcode))
+            /*if (Input.GetKeyDown(kcode))
             {
                 //print(kcode);
                 data.player_code.Add(kcode);
-                
-                //Debug.Log(PlayerData.Static.player_code);
-                //Debug.Log("KeyCode down: " + kcode);
-                /*if (kcode!= PlayerData.Static.player_code)
-                {
-                    Debug.Log("19");
-                }*/
 
-            }
-            }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            foreach (var item in data.player_code)
-            {
-                item.ToString();
->>>>>>> 0cf2bb9bb665de4924a12d39676e10aeb95d3253
+            }*/
             }
 
         }
 
     }
-<<<<<<< HEAD
-
-=======
     /*public void AllPlayerKey()
     {
         KeyData.Add(new PlayerData() { })
     }*/
->>>>>>> 0cf2bb9bb665de4924a12d39676e10aeb95d3253
+
 
 }
 
