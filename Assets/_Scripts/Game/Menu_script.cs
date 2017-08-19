ccf8d79427e4a2be53a9fb548f3c12964a651368
ivanl1234 <leeholun1023@gmail.com>
2017-08-19 19:41:38 +0800

    no message

--- Assets/_Scripts/Game/Menu_script.cs
+++ Assets/_Scripts/Game/Menu_script.cs
@@ -6,20 +6,34 @@ using UnityEngine.UI;
 
 public class Menu_script : MonoBehaviour
 {
-
+    public bool alphabetlock = false;
 	public List<PlayerData> _players;
+    public GameObject test_object;
 
-	void Start()
+    void Start()
     {
 		_players = new List<PlayerData> ();
 	}
 
     public void Update()
     {
+
         foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))//Get what key are you pressing
         {
-			if (Input.GetKeyDown (kcode)) {
+			if (Input.GetKeyDown (kcode))
+           {
+                if((int)kcode==27)//get escape key
+                {
+                    Debug.Log("esc");
+                }
 
+                if((int)kcode==13)//get enter key
+                {
+                    Debug.Log("enter");
+
+                }
+                if( (int)kcode>97&&(int)kcode<122)// only alphabet
+                { 
 				bool found = false;
 
 				for(int i = _players.Count -1; i > -1; i--) {
@@ -27,6 +41,7 @@ public class Menu_script : MonoBehaviour
 					if (p.player_code == kcode) {
 						_players.Remove (p);
 						found = true;
+
 					}
 				}
 
@@ -36,9 +51,9 @@ public class Menu_script : MonoBehaviour
 					newPlayer._color = Color.red;
 					_players.Add (newPlayer);
 				}
+                }
 
-
-			}
+            }
 
         }
 
@@ -53,7 +68,7 @@ public class Menu_script : MonoBehaviour
 //        }
 
     }
- 
+
 
 }
 
