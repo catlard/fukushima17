6398002cfbcb8fdc72cc45b4bc1f70d43ad598fd
Simon Braunstein <sgbraunstein@gmail.com>
2017-08-20 09:48:57 +0800

    God can make fake players.

--- Assets/God.cs
+++ Assets/God.cs
@@ -5,8 +5,27 @@ using UnityEngine;
 public class God : SingletonMonoBehavior<God> {
 
 	public List<PlayerData> _players; 
+	public int _fakePlayersAtStart = 0;
+
 
 	public void Start() {
 		DontDestroyOnLoad (gameObject);
+		if (_fakePlayersAtStart < 1)
+			return;
+		MakeFakePlayers (_fakePlayersAtStart);
+	}
+
+
+	public void MakeFakePlayers(int howMany) {
+		_players = new List<PlayerData> ();
+
+		for (int i = 0; i < howMany; i++) {
+			PlayerData d = new PlayerData ();
+			d.color = new Color (Random.value, Random.value, Random.value);
+			d.player_code = (KeyCode) (i + 40);
+			print ("GOD: Made fake player with code: " + d.player_code);
+			d.score = Random.Range (1, 10);
+			_players.Add (d);
+		}
 	}
 }
